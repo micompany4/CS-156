7ebad068332e8328f28ce57a2710cf1a6e0d4023
Joseph Nguyen <joseph.q.nguyen@sjsu.edu>
2019-11-07 22:43:30 -0800

    asd

--- hw5/sudoku.py
+++ hw5/sudoku.py
@@ -2,7 +2,11 @@
 # Name:     sudoku
 # Purpose:  Homework5
 #
-# Author(s): Joseph Nguyen, Michael Wong
+<<<<<<< HEAD
+# Author(s):
+=======
+# Author(s): Michael Wong, Joseph Nguyen
+>>>>>>> 583f43dbc295ad922cee2ba64de0c3e4049a3a4d
 #
 # ----------------------------------------------------------------------
 """
@@ -14,20 +18,12 @@ q3:  Backtracking Search with MRV Ordering and AC-3
 """
 import csp
 
-
 # Enter your helper functions here
-def constraint(var1, val1, var2, val2):
-    return not val1 == val2
 
+<<<<<<< HEAD
+=======
 
-def region(row_min, col_min):
-    tuple_set = set()
-    for i in range(row_min, row_min + 3):
-        for j in range(col_min, col_min + 3):
-            tuple_set.add((i, j))
-    return tuple_set
-
-
+>>>>>>> 583f43dbc295ad922cee2ba64de0c3e4049a3a4d
 def build_csp(puzzle):
     """
     Create a CSP object representing the puzzle.
@@ -36,36 +32,13 @@ def build_csp(puzzle):
     are the corresponding numbers assigned to these squares.
     :return: CSP object
     """
-    indexes = []
-    for i in range(0, 9):
-        for j in range(0, 9):
-            indexes.append((i, j))
-
-    index_domains = {}
-    for i in indexes:
-        if i in puzzle:
-            index_domains[i] = {puzzle[i]}
-        else:
-            index_domains[i] = {1, 2, 3, 4, 5, 6, 7, 8, 9}
-
-    index_neighbors = {}
-    for i in indexes:
-        (row, col) = i
-
-        #  regions
-        row_min = row // 3 * 3  # gives the starting index of region
-        col_min = col // 3 * 3
-        neighbors = region(row_min, col_min)
-
-        #  rows and columns
-        for j in range(0, 9):
-            neighbors.add((row, j))
-            neighbors.add((j, col))
-
-        neighbors = neighbors - {i}  # remove itself from neighbors
-        index_neighbors[i] = neighbors
-
-    return csp.CSP(index_domains, index_neighbors, constraint)
+    # Enter your code here and remove the pass statement below
+<<<<<<< HEAD
+    pass
+=======
+    csp_object = csp(puzzle, {'key': 2.23, 'seed': 9.43}, 0) # some random values
+    return csp_object
+>>>>>>> 583f43dbc295ad922cee2ba64de0c3e4049a3a4d
 
 
 def q1(puzzle):
@@ -77,10 +50,16 @@ def q1(puzzle):
     :return: a tuple consisting of a solution (dictionary) and the
     CSP object.
     """
-    the_csp = build_csp(puzzle)
-    solution = the_csp.backtracking_search()
-    return solution, the_csp
+    # Enter your code here and remove the pass statement below
+<<<<<<< HEAD
+    pass
+=======
+    csp_obj = build_csp(puzzle)
+    result = (csp_obj.backtracking_search(), csp_obj)
 
+    return result
+
+>>>>>>> 583f43dbc295ad922cee2ba64de0c3e4049a3a4d
 
 def q2(puzzle):
     """
@@ -92,11 +71,16 @@ def q2(puzzle):
     :return: a tuple consisting of a solution (dictionary) and the
     CSP object.
     """
-    the_csp = build_csp(puzzle)
-    the_csp.ac3_algorithm()
-    solution = the_csp.backtracking_search()
-    return solution, the_csp
+    # Enter your code here and remove the pass statement below
+<<<<<<< HEAD
+    pass
+=======
+    csp_obj = build_csp(puzzle)
+    csp_obj.ac3_algorithm()
+    result = (csp_obj.backtracking_search(), csp_obj)
 
+    return result
+>>>>>>> 583f43dbc295ad922cee2ba64de0c3e4049a3a4d
 
 def q3(puzzle):
     """
@@ -108,7 +92,13 @@ def q3(puzzle):
     :return: a tuple consisting of a solution (dictionary) and the
     CSP object.
     """
-    the_csp = build_csp(puzzle)
-    the_csp.ac3_algorithm()
-    solution = the_csp.backtracking_search("MRV")
-    return solution, the_csp
+    # Enter your code here and remove the pass statement below
+<<<<<<< HEAD
+    pass
+=======
+    csp_obj = build_csp(puzzle)
+    csp_obj.ac3_algorithm()
+    result = (csp_obj.backtracking_search("MRV"), csp_obj)
+
+    return result
+>>>>>>> 583f43dbc295ad922cee2ba64de0c3e4049a3a4d
---
+++ hw6/adversarial_search.py
@@ -0,0 +1,228 @@
+# ----------------------------------------------------------------------
+# Name:     adversarial_search
+# Purpose:  Homework 6 - Implement adversarial search algorithms
+#
+# Authors: Joseph Nguyen, Michael Wong
+#
+# ----------------------------------------------------------------------
+"""
+Adversarial search algorithms implementation
+Your task for homework 6 is to implement:
+1.  minimax
+2.  alphabeta
+3.  abdl (alpha beta depth limited)
+"""
+import random
+import math  # You can use math.inf to initialize to infinity
+import math
+
+
+def rand(game_state):
+    """
+    Generate a random move.
+    :param game_state: GameState object
+    :return:  a tuple representing the row column of the random move
+    """
+    done = False
+    while not done:
+        row = random.randint(0, game_state.size - 1)
+        col = random.randint(0, game_state.size - 1)
+        if game_state.available(row, col):
+            done = True
+    return row, col
+
+
+def minimax(game_state):
+    """
+    Find the best move for our AI agent using the minimax algorithm.
+    (searching the entire tree from the current game state)
+    :param game_state: GameState object
+    :return:  a tuple representing the row column of the best move
+    """
+    # Enter your code here and remove the raise statement below
+    # return max(value(game_state.successor(m, 'AI'), 'user') for m in game_state.possible_moves())
+    return max(game_state.possible_moves(), key=lambda node: value(game_state.successor(node, 'AI'), 'user'))
+
+
+def value(game_state, agent):
+    """
+    Calculate the minimax value for any state under the given agent's
+    control.
+    :param game_state: GameState object - state may be terminal or
+    non-terminal
+    :param agent: (string) 'user' or 'AI' - AI is max
+    :return: (integer) value of that state -1, 0 or 1
+    """
+    # Enter your code here and remove the pass statement below
+    if game_state.is_win('AI'):
+        return 1
+    elif game_state.is_tie():
+        return 0
+    elif game_state.is_win('user'):
+        return -1
+    elif agent is 'AI':
+        return max_value(game_state)
+    else:
+        return min_value(game_state)
+
+
+def max_value(game_state):
+    """
+    Calculate the minimax value for a non-terminal state under Max's
+    control (AI agent)
+    :param game_state: non-terminal GameState object
+    :return: (integer) value of that state -1, 0 or 1
+    """
+    # Enter your code here and remove the pass statement below
+    # return max(game_state.possible_moves(), key=lambda m: value(game_state.successor(m, 'AI'), 'user'))
+    return max(value(game_state.successor(m, 'AI'), 'user') for m in game_state.possible_moves())
+
+
+def min_value(game_state):
+    """
+    Calculate the minimax value for a non-terminal state under Min's
+    control (user)
+    :param game_state: non-terminal GameState object
+    :return: (integer) value of that state -1, 0 or 1
+    """
+    # Enter your code here and remove the pass statement below
+    # return min(game_state.possible_moves(), key=lambda m: value(game_state.successor(m, 'user'), 'AI'))
+    return min(value(game_state.successor(m, 'user'), 'AI') for m in game_state.possible_moves())
+
+
+def alphabeta(game_state):
+    """
+    Find the best move for our AI agent using the minimax algorithm
+    with alpha beta pruning.
+    :param game_state: GameState object
+    :return:  a tuple representing the row column of the best move
+    """
+    # Enter your code here and remove the raise statement below
+    return max(game_state.possible_moves(), key=lambda node: ab_value(game_state.successor(node, 'AI'), 'user',
+                                                                      -math.inf, math.inf))
+
+
+def ab_value(game_state, agent, alpha, beta):
+    """
+    Calculate the minimax value for any state under the given agent's
+    control using alpha beta pruning
+    :param game_state: GameState object - state may be terminal or
+    non-terminal.
+    :param agent: (string) 'user' or 'AI' - AI is max
+    :return: (integer) value of that state -1, 0 or 1
+    """
+    # Enter your code here and remove the pass statement below
+    if game_state.is_win('AI'):
+        return 1
+    elif game_state.is_tie():
+        return 0
+    elif game_state.is_win('user'):
+        return -1
+    elif agent is 'AI':
+        return abmax_value(game_state, alpha, beta)
+    else:
+        return abmin_value(game_state, alpha, beta)
+
+
+def abmax_value(game_state, alpha, beta):
+    """
+    Calculate the minimax value for a non-terminal state under Max's
+    control (AI agent) using alpha beta pruning
+    :param game_state: non-terminal GameState object
+    :return: (integer) value of that state -1, 0 or 1
+    """
+    # Enter your code here and remove the pass statement below
+    v = -math.inf
+    for s in game_state.possible_moves():
+        v = max(v, ab_value(game_state.successor(s, 'AI'), 'user', alpha, beta))
+        if v >= beta:
+            return v
+        alpha = max(alpha, v)
+    return v
+
+
+def abmin_value(game_state, alpha, beta):
+    """
+    Calculate the minimax value for a non-terminal state under Min's
+    control (user) using alpha beta pruning
+    :param game_state: non-terminal GameState object
+    :return: (integer) value of that state -1, 0 or 1
+    """
+    # Enter your code here and remove the pass statement below
+    v = math.inf
+    for s in game_state.possible_moves():
+        v = min(v, ab_value(game_state.successor(s, 'user'), 'AI', alpha, beta))
+        if v <= alpha:
+            return v
+        beta = min(beta, v)
+    return v
+
+
+def abdl(game_state, depth):
+    """
+    Find the best move for our AI agent by limiting the alpha beta
+    search the given depth and using the evaluation function
+    game_state.eval()
+    :param game_state: GameState object
+    :return:  a tuple representing the row column of the best move
+    """
+    return max(game_state.possible_moves(), key=lambda node: abdl_value(game_state.successor(node, 'AI'), 'user',
+                                                                        -math.inf, math.inf, depth))
+
+
+def abdl_value(game_state, agent, alpha, beta, depth):
+    """
+    Calculate the utility for any state under the given agent's control
+    using depth limited alpha beta pruning and the evaluation
+    function game_state.eval()
+    :param game_state: GameState object - state may be terminal or
+    non-terminal
+    :param agent: (string) 'user' or 'AI' - AI is max
+    :return: (integer) utility of that state
+    """
+    if game_state.is_win('AI'):
+        return 2 * game_state.size + 2
+    elif game_state.is_tie():
+        return 0
+    elif game_state.is_win('user'):
+        return - 2 * game_state.size - 2
+    elif depth == 0:
+        return game_state.eval()
+    elif agent is 'AI':
+        return abdlmax_value(game_state, alpha, beta, depth)
+    else:
+        return abdlmin_value(game_state, alpha, beta, depth)
+
+
+def abdlmax_value(game_state, alpha, beta, depth):
+    """
+    Calculate the utility for a non-terminal state under Max's control
+    using depth limited alpha beta pruning and the evaluation
+    function game_state.eval()
+    :param game_state: non-terminal GameState object
+    :return: (integer) utility (evaluation function) of that state
+    """
+    v = -math.inf
+    for s in game_state.possible_moves():
+        v = max(v, abdl_value(game_state.successor(s, 'AI'), 'user', alpha, beta, depth - 1))
+        if v >= beta:
+            return v
+        alpha = max(alpha, v)
+    return v
+
+
+def abdlmin_value(game_state, alpha, beta, depth):
+    """
+    Calculate the utility for a non-terminal state under Min's control
+    using depth limited alpha beta pruning and the evaluation
+    function game_state.eval()
+    :param game_state: non-terminal GameState object
+    :return: (integer) utility (evaluation function) of that state
+    """
+    v = math.inf
+    for s in game_state.possible_moves():
+        v = min(v, abdl_value(game_state.successor(s, 'user'), 'AI', alpha, beta, depth - 1))
+        if v <= alpha:
+            return v
+        beta = min(beta, v)
+    return v
